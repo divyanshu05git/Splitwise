7e77a5af407be0cc704615c368fc10ee51dcdd63
divyanshu05git <divyanshupratapsingh05@gmail.com>
2026-03-06 11:05:34 +0530

    expense chart added succesfully but with raw data

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
+import ExpenseChart from "@/components/ExpenseChart";
 
 
 export default function Dashboard() {
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-           DashBoard
+           <ExpenseChart/>
         </div>
     );
 }
--- frontend/components/ExpenseChart.tsx
+++ frontend/components/ExpenseChart.tsx
@@ -1,7 +1,72 @@
-"use Client"
+"use client"
 
 import ReactECharts from "echarts-for-react";
+import { EChartsOption } from "echarts";
 
 export default function ExpenseChart() {
+
+    const options:EChartsOption={
+        title:{
+            text:"Expenses",
+            left:"center",
+            top:20,
+            textStyle:{
+                fontSize:16,
+                color:"#333"
+            }
+        },
+
+        tooltip:{
+            trigger:"axis",
+        },
+
+        xAxis:{
+            type:"category",
+            data:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+            axisTick:{
+                show:false
+            },
+            axisLine:{
+                show:false
+            },
+            axisLabel:{
+                margin:15,
+                fontWeight:500,
+                color:"#94a3b8"
+            }
+
+        },
+
+        yAxis:{
+            type:'value',
+            min:100,
+            minInterval:100,
+            axisLabel:{
+                show:false,
+            },
+            splitLine:{
+                show:false,
+            },
+        },
+
+        series:[{
+            type:"bar",
+            data:[200,300,150,400,250,350,300,450,500,400,350,600],
+            barWidth:"60%",
+            itemStyle:{
+                color: "#e5e7eb",
+                borderRadius:[10,10,10,10],
+            },
+            emphasis:{
+                itemStyle:{
+                    color: "#2563eb"
+                }
+            }
+        }]
+    }
+
+    return (
+        <ReactECharts option={options} style={{height:"100%",width:"60%"}} /> 
+    )
 
 }
